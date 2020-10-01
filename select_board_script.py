0917c4e67396bd081b0e34814486578e76c20a14
Salem-Luos <thomas.salembier@luos.io>
2020-10-01 17:35:56 +0200

    change script to find luosHAL

--- select_board_script.py
+++ select_board_script.py
@@ -3,7 +3,7 @@ from os.path import join, realpath
 
 # private library flags
 for item in env.get("CPPDEFINES", []):
-    if isinstance(item, tuple) and item[0] == "HAL":
+    if isinstance(item, tuple) and item[0] == "LUOSHAL":
         env.Append(CPPPATH=[realpath(item[1])])
         env.Replace(SRC_FILTER=[ "+<%s>" % item[1]])
         break
