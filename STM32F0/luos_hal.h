03eaa391d5bf7f425471af2fd84cad8d4525eda3
Salem-Luos <thomas.salembier@luos.io>
2021-01-12 15:39:12 +0100

    Timeout F0
    Timeout F0

--- STM32F0/luos_hal.h
+++ STM32F0/luos_hal.h
@@ -15,11 +15,11 @@
  * Definitions
  ******************************************************************************/
 #define LUOS_UUID ((uint32_t *)0x1FFFF7AC)
-#define MCUFREQ 48000000
+
+#define TIMER_RELOAD_CNT 20
 
 #define ADDRESS_ALIASES_FLASH ADDRESS_LAST_PAGE_FLASH
 #define ADDRESS_BOOT_FLAG_FLASH (ADDRESS_LAST_PAGE_FLASH + PAGE_SIZE) - 4
-
 /*******************************************************************************
  * Variables
  ******************************************************************************/
