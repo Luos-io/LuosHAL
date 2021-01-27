a1e9ee84315ea56d46169d320a2208083e0e5f3f
Salem-Luos <thomas.salembier@luos.io>
2021-01-27 19:14:28 +0100

    Change Timeout when change Baudrate G4

--- STM32G4/luos_hal_config.h
+++ STM32G4/luos_hal_config.h
@@ -15,9 +15,8 @@
 #define DISABLE 0x00
 
 #define MCUFREQ 170000000
+#define TIMERDIV 1
 
-//Timer 1us step at 1Mhz usart
-#define TIMERPRESCALER   (MCUFREQ/DEFAULTBAUDRATE)/1//(freq MCU/freq timer)/divider timer clock source
 /*******************************************************************************
  * PINOUT CONFIG
  ******************************************************************************/
