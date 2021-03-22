708158c4379de18cd692598cd71e8894a2b79864
Salem_Luos <thomas.salembier@luos.io>
2021-03-22 19:15:57 +0100

    SAMD21 ARDUINO Tx IT and DMA

--- ATSAMD21_ARDUINO/board_config.h
+++ ATSAMD21_ARDUINO/board_config.h
@@ -12,31 +12,33 @@
 
 #include <Arduino.h>
 
-#ifdef ARDUINO_SAMD_ZERO
-#define LUOS_COM_CLOCK_ENABLE()                                                                                   \
-  do                                                                                                              \
-  {                                                                                                               \
-    GCLK->CLKCTRL.reg =                                                                                           \
-    (uint16_t)(GCLK_CLKCTRL_ID(GCLK_CLKCTRL_ID_SERCOM0_CORE_Val) | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_CLKEN);  \
-    PM->APBCMASK.reg |= PM_APBCMASK_SERCOM0;                                                                      \
-  } while (0U)
-#define LUOS_COM SERCOM0
-#define LUOS_COM_IRQ SERCOM0_IRQn
-#define LUOS_COM_IRQHANDLER() SERCOM0_Handler()
+#if defined(ARDUINO_SAMD_ZERO)
+  #define LUOS_COM_CLOCK_ENABLE()                                                                                   \
+    do                                                                                                              \
+    {                                                                                                               \
+      GCLK->CLKCTRL.reg =                                                                                           \
+      (uint16_t)(GCLK_CLKCTRL_ID(GCLK_CLKCTRL_ID_SERCOM0_CORE_Val) | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_CLKEN);  \
+      PM->APBCMASK.reg |= PM_APBCMASK_SERCOM0;                                                                      \
+    } while (0U)
+  #define LUOS_COM SERCOM0
+  #define LUOS_COM_IRQ SERCOM0_IRQn
+  #define LUOS_COM_IRQHANDLER() SERCOM0_Handler()
 #endif
 
-#if defined(ARDUINO_SAMD_MKR1000) || defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_MKRFox1200) || defined(ARDUINO_SAMD_MKRWAN1300) \
-|| defined(ARDUINO_SAMD_MKRWAN1310)|| defined(ARDUINO_SAMD_MKRGSM1400) || defined(ARDUINO_SAMD_MKRNB1500) 
-.
-  do                                                                                                              \
-  {                                                                                                               \
-    GCLK->CLKCTRL.reg =                                                                                           \
-    (uint16_t)(GCLK_CLKCTRL_ID(GCLK_CLKCTRL_ID_SERCOM5_CORE_Val) | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_CLKEN);  \
-    PM->APBCMASK.reg |= PM_APBCMASK_SERCOM5                                                                      \
-  } while (0U)
-#define LUOS_COM SERCOM5
-#define LUOS_COM_IRQ SERCOM5_IRQn
-#define LUOS_COM_IRQHANDLER() SERCOM5_Handler()
+#if (defined(ARDUINO_SAMD_MKR1000) || defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_MKRFox1200)         \
+    || defined(ARDUINO_SAMD_MKRWAN1300) || defined(ARDUINO_SAMD_MKRWAN1310)|| defined(ARDUINO_SAMD_MKRGSM1400)      \
+    || defined(ARDUINO_SAMD_MKRNB1500) || defined(ARDUINO_SAMD_MKRZERO) || defined(ARDUINO_SAMD_NANO_33_IOT)        \
+    || defined(SAMD_MKRVIDOR4000))
+  #define LUOS_COM_CLOCK_ENABLE()\
+    do                                                                                                              \
+    {                                                                                                               \
+      GCLK->CLKCTRL.reg =                                                                                           \
+      (uint16_t)(GCLK_CLKCTRL_ID(GCLK_CLKCTRL_ID_SERCOM5_CORE_Val) | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_CLKEN);  \
+      PM->APBCMASK.reg |= PM_APBCMASK_SERCOM5;                                                                      \
+    } while (0U)
+  #define LUOS_COM SERCOM5
+  #define LUOS_COM_IRQ SERCOM5_IRQn
+  #define LUOS_COM_IRQHANDLER() SERCOM5_Handler()
 #endif
 
 #endif /* _BOARD_CONFIG_H_ */
