a64e55141b23bd5605893a0f2f83ee5c2aa275ec
Salem_Luos <thomas.salembier@luos.io>
2021-04-15 12:15:27 +0200

    F0 PATCH by adding a tempo before send Ack

--- STM32F0/luos_hal.c
+++ STM32F0/luos_hal.c
@@ -24,6 +24,7 @@
  * Definitions
  ******************************************************************************/
 #define DEFAULT_TIMEOUT 20
+#define TIMEOUT_ACK DEFAULT_TIMEOUT/4
 /*******************************************************************************
  * Variables
  ******************************************************************************/
@@ -210,7 +211,7 @@ void LuosHAL_SetRxState(uint8_t Enable)
 {
     if (Enable == true)
     {
-    	LL_USART_RequestRxDataFlush(LUOS_COM);  // Clear data register
+        LL_USART_RequestRxDataFlush(LUOS_COM);  // Clear data register
         LL_USART_EnableIT_RXNE(LUOS_COM);// Enable Rx IT
     }
     else
@@ -285,8 +286,6 @@ void LuosHAL_ComTransmit(uint8_t *data, uint16_t size)
     while (LL_USART_IsActiveFlag_TXE(LUOS_COM) == RESET)
         ;
     // Disable RX detec pin if needed
-    // Enable TX
-    LuosHAL_SetTxState(true);
 
     // Reduce size by one because we send one directly
     data_size_to_transmit = size - 1;
@@ -313,6 +312,8 @@ void LuosHAL_ComTransmit(uint8_t *data, uint16_t size)
     LL_DMA_SetDataLength(LUOS_DMA, LUOS_DMA_CHANNEL, size);
     // set request DMA
     LL_USART_EnableDMAReq_TX(LUOS_COM);
+            // Enable TX
+    LuosHAL_SetTxState(true);
     // Enable DMA again
     LL_DMA_EnableChannel(LUOS_DMA, LUOS_DMA_CHANNEL);
     // enable transmit complete
@@ -321,6 +322,10 @@ void LuosHAL_ComTransmit(uint8_t *data, uint16_t size)
     }
     else
     {
+        //wait before send ack
+        while(LL_TIM_GetCounter(LUOS_TIMER) < TIMEOUT_ACK);//this is a patch du to difference MCU frequency
+                // Enable TX
+        LuosHAL_SetTxState(true);
         // Transmit the only byte we have
         LL_USART_TransmitData8(LUOS_COM, *data);
         // Enable Transmission complete interrupt because we only have one.
