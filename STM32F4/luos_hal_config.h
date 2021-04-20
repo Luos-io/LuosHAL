b6e68650ceb1f8dbddf0cb21c8a39e8be8b0b27e
Salem_Luos <thomas.salembier@luos.io>
2021-04-20 10:58:48 +0200

    F4 define HW CRC

--- STM32F4/luos_hal_config.h
+++ STM32F4/luos_hal_config.h
@@ -12,10 +12,13 @@
 
 #include "stm32f4xx_hal.h"
 
-//If your MCU do not Have DMA for tx transmit define USE_TX_IT
-//If your MCU have CRC polynome 16 #define USE_CRC_HW
-
 #define DISABLE 0x00
+
+//If your MCU do not Have DMA for tx transmit define USE_TX_IT
+//If your MCU have CRC polynome 16 #define USE_CRC_HW 1 else #define USE_CRC_HW 0
+#ifndef USE_CRC_HW
+#define USE_CRC_HW 0
+#endif
 
 #ifndef MCUFREQ
 #define MCUFREQ 168000000 //MCU frequence 168000000
