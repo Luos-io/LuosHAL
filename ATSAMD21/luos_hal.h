645f3466e17079004b72e1ea35b052e80bf83e4b
nicolas cros <nicolas.cros@luos.io>
2021-10-08 17:29:20 +0200

    implement timestamp on ATSAMD21

--- ATSAMD21/luos_hal.h
+++ ATSAMD21/luos_hal.h
@@ -28,6 +28,13 @@
  * Variables
  ******************************************************************************/
 
+typedef struct ll_timestamp
+{
+    uint32_t lower_timestamp;
+    uint64_t higher_timestamp;
+    uint64_t start_offset;
+} ll_timestamp_t;
+
 /*******************************************************************************
  * Function
  ******************************************************************************/
@@ -62,5 +69,10 @@ uint16_t LuosHAL_GetNodeID(void);
 void LuosHAL_EraseMemory(uint32_t, uint16_t);
 void LuosHAL_ProgramFlash(uint32_t, uint16_t, uint8_t *);
 #endif
+
+// timestamp functions
+uint64_t LuosHAL_GetTimestamp(void);
+void LuosHAL_StartTimestamp(void);
+void LuosHAL_StopTimestamp(void);
 
 #endif /* _LUOSHAL_H_ */
